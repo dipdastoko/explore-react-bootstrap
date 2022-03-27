cc66977d9f7252235404560e2208b81297ac22a2
dipdastoko <dipdastoko@gmail.com>
2022-03-28 00:53:01 +0600

    bootstap in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card/Card';
 
 function App() {
+  const items = [
+    {
+      name: 'first item',
+      description: 'slkf',
+      img: 'https://thumbs.dreamstime.com/b/concrete-mixer-classic-big-rig-semi-truck-evening-road-white-red-wings-straight-abutting-horizon-their-49976228.jpg'
+    },
+    {
+      name: 'first item',
+      description: 'slkf',
+      img: 'https://thumbs.dreamstime.com/b/concrete-mixer-classic-big-rig-semi-truck-evening-road-white-red-wings-straight-abutting-horizon-their-49976228.jpg'
+    },
+    {
+      name: 'first item',
+      description: 'slkf',
+      img: 'https://thumbs.dreamstime.com/b/concrete-mixer-classic-big-rig-semi-truck-evening-road-white-red-wings-straight-abutting-horizon-their-49976228.jpg'
+    },
+    {
+      name: 'first item',
+      description: 'slkf',
+      img: 'https://thumbs.dreamstime.com/b/concrete-mixer-classic-big-rig-semi-truck-evening-road-white-red-wings-straight-abutting-horizon-their-49976228.jpg'
+    },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row row-cols-1 row-cols-md-4 g-4">
+      {
+        items.map(item => <Card
+          item={item}
+        ></Card>)
+      }
     </div>
   );
 }
