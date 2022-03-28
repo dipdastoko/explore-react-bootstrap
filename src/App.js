cedc35d387811fdfcd91e312078488c3d08e427c
dipdastoko <dipdastoko@gmail.com>
2022-03-28 12:45:15 +0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Card from './components/Card/Card';
 import { Button, Col, Row, Spinner } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
 import News from './components/News/News';
@@ -13,12 +12,15 @@ function App() {
       .then(data => setNews(data.articles))
   }, [])
   return (
-    <div className='App'>
-      <Row xs={1} md={2} className="g-4">
-        {
-          news.map(nw => <News news={nw}></News>)
-        }
-      </Row>
+    <div>
+      {news.length === 0 ?
+        <Spinner animation="border" />
+        :
+        <Row xs={1} md={3} className="g-4">
+          {
+            news.map(nw => <News news={nw}></News>)
+          }
+        </Row>}
     </div >
   );
 }
